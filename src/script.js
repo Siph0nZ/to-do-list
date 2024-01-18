c332a6c38f1926c33dc66faf9dd234369adaa4ec
Siph0nZ <garrodmason@gmail.com>
2024-01-18 11:55:18 +0000

    added core functionality

---
+++ src/script.js
@@ -0,0 +1,28 @@
+// Function to add a task \\
+function addTask() {
+  // Get the task from the input \\
+  var taskInput = document.getElementById("taskInput");
+  var taskList = document.getElementById("taskList");
+
+  // Gets value entered by taskInput
+  var newTaskValue = taskInput.value;
+
+  // Create a new list item \\
+  var newTaskItem = document.createElement("li");
+  newTaskItem.innerText = newTaskValue;
+
+  // Checks if the input was empty
+  if (newTaskValue === "") {
+    alert("You must enter a task!");
+  } else {
+    // Append the next list item to the task list
+    taskList.appendChild(newTaskItem);
+  }
+
+  // Clear the input field
+  taskInput.value = "";
+}
+
+// Attach the addTask function to the button click event
+var addButton = document.querySelector("button");
+addButton.addEventListener("click", addTask);
