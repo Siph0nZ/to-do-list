17795d363296ab913fe01fb0b4370917021c8c45
Siph0nZ <garrodmason@gmail.com>
2024-01-18 18:12:34 +0000

    added remove button

--- src/script.js
+++ src/script.js
@@ -1,24 +1,54 @@
 // Function to add a task \\
 function addTask() {
-  // Get the task from the input \\
-  var taskInput = document.getElementById("taskInput");
+  // Declare Variables
+  var taskInput = document.getElementById("taskInput"); // Get the task from the input \\
   var taskList = document.getElementById("taskList");
+  var newTaskValue = taskInput.value; // Gets value entered by taskInput
+  var maxChar = 50; // Max characters for the input
 
-  // Gets value entered by taskInput
-  var newTaskValue = taskInput.value;
-
-  // Create a new list item \\
+  // Create a new list item
   var newTaskItem = document.createElement("li");
   newTaskItem.innerText = newTaskValue;
+
+  // Create a delete button for each task
+  var deleteButton = document.createElement("button");
+  deleteButton.innerText = "Remove";
+  deleteButton.className = "remove-button"; // CSS class
+  deleteButton.onclick = function () {
+    removeTask(newTaskItem);
+  };
+
+  // Create a edit button for each task
+  var editButton = document.createElement("button");
+  editButton.innerText = "Edit";
+  editButton.className = "edit-button"; // CSS class
+  editButton.onclick = function () {
+    editTask(newTaskItem);
+  };
 
   // Checks if the input was empty
   if (newTaskValue === "") {
     alert("You must enter a task!");
-  } else {
+
+    // Checks if the input was too long
+  } else if (newTaskValue.length > maxChar) {
+    newTaskValue.length > maxChar;
+    alert("Your task must be less than 50 characters!");
+
     // Append the next list item to the task list
+  } else {
     taskList.appendChild(newTaskItem);
+    newTaskItem.appendChild(deleteButton);
+    newTaskItem.appendChild(editButton);
   }
 
   // Clear the input field
   taskInput.value = "";
 }
+
+// Function to remove a task \\
+function removeTask(taskItem) {
+  taskItem.parentNode.removeChild(taskItem);
+}
+
+// Function to edit a task \\
